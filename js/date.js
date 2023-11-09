c0fe76c63c2acc11869ca6839abe1e0db77845a9
NastinNN <nastya.mannova@gmail.com>
2023-11-09 20:36:18 +0300

    Решены задачи 1, 2 и 3

--- js/date.js
+++ js/date.js
@@ -16,3 +16,24 @@ function task2() {
       let koldayNY=(dataNY-data)/1000/60/60/24;
       console.log(`До нового года осталось ${koldayNY.toFixed(0)} дня`)
 }
+
+function task3() {
+      let ms=prompt("Введите количество милисекунд");
+      if(Number.isInteger(ms) || ms<0 || isNaN(ms))
+        alert("Некорректный ввод данных");
+      else 
+      {
+      let day=Math.floor(+ms /(1000*60*60*24));
+      let hour=Math.floor((+ms -(day*1000*60*60*24))/(1000*60*60));
+      let minute=Math.floor((+ms -(day*1000*60*60*24)-(hour*1000*60*60))/(1000*60));
+      let second=Math.floor((+ms -(day*1000*60*60*24)-(hour*1000*60*60)-(minute*1000*60))/1000);
+      let dataObj={
+            d: day,
+            h: hour,
+            m: minute,
+            s: second,
+            }
+      console.log(dataObj);
+      }
+}
+
